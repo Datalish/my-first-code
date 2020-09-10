58674327587a80e263c7e218975734d439f01d85
Darlyn Talia <darlinta15@gmail.com>
2020-09-09 19:46:36 -0500

    Document object model

---
+++ dom/codigo.js
@@ -0,0 +1,4 @@
+var ruta = window.location;
+        console.log("con esta forma puedo dejar mensajes en la consola");
+        console.log(ruta);
+        document.write("Estás en: " + ruta.origin);
